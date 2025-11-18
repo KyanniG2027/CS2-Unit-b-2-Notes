e08120d0203fb5992aa35113191e9812353e5208
Kyanni <kyanni.gonzalez2027@gbwl.org>
2025-11-18 16:06:36 +0000

    2.8 Notes

--- Main.java
+++ Main.java
@@ -16,11 +16,35 @@ public class Main {
          //Step #3: Change the variable
          // (do this after the statements you want to repeat)
          count++;
-
-
-
-
       }
 
-   }
+         //FOR LOOPS: when you know HOW MANY TIMES to iterate 
+         // for (initialize; test condition; change)
+
+         for (int i = 5; i <= 27; i++) {
+            System.out.println(i);  
+         }
+
+         // Modify the CHANGE step to skip certain numbers
+         for ( int i = 0; i<= 10; i+=2) {
+         System.out.println(i);
+         }
+         //Watch your INITIAL value and your OPERATOR 
+         for ( int i = 0; i < 11; i += 2) {
+            System.out.println(i);
+         }
+       //DECREMENTING for loops are like counting backwards
+       for (int i = 10; i >=0; i--){
+       System.out.println(i);
+       }
+       //Print a special message when i is 1
+
+       if (i==1){
+         System.out.println("Blast Off!🚀");
+       }
+
+
+
+   }  
+
 }
